ebb62588134ac8a6ba3282b2f95795ccb9e69b51
abhirampathri <145159726+abhirampathri@users.noreply.github.com>
2024-06-12 22:45:57 -0400

    keypad

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,25 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] private Text Ans;
+
+    private string Answer = "1423";
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+
+    public void Execute()
+    {
+        if (Ans.text == Answer){
+
+        } else {
+            Ans.text = "INVALID";
+        }
+    }
+}
